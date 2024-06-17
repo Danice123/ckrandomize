dc63a9175f33be3da7ef58420009f63bd03c9e72
Danice123 <mitchelldanice@gmail.com>
2024-06-17 01:24:28 -0400

    extension

---
+++ extension/tool.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button').addEventListener('click', async () => {
+        var file = document.getElementById('docfile').files[0]
+        if (!file) {
+            return
+        }
+
+        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+        
+        var reader = new FileReader()
+        reader.onload = function(e) {
+          var j = JSON.parse(e.target.result)
+          chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: (j) => {
+                window['data'].encounters = j.encounters
+                window['data'].encounter_pools = j.encounter_pools
+                window['encounterPools'] = j.encounter_pools
+            },
+            args: [j],
+            world: 'MAIN',
+          })
+        };
+        reader.readAsText(file)
+    });
+});
